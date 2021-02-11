5218f8d5eaffab3693eb36741198e0ff2da59a7f
Cameron <mcleancam22@gmail.com>
2021-02-10 22:13:43 -0500

    Set Survey Done

--- Game1011_Assignment1_PatrickCrowley/Main.cpp
+++ Game1011_Assignment1_PatrickCrowley/Main.cpp
@@ -1,6 +1,10 @@
 #include "Survey.h"
-
 int main()
 {
+	int temp;
+	std::cout << "Please enter amount of students to create: ";
+	cin >> temp;
+	Survey StudentSurvey(temp);
 
+	StudentSurvey.DisplaySurvey();
 }
--- Game1011_Assignment1_PatrickCrowley/Survey.h
+++ Game1011_Assignment1_PatrickCrowley/Survey.h
@@ -5,13 +5,28 @@ class Survey
 {
 public:
 	//Constuctor
-
+	Survey(int surveyNumb)
+	{
+		setSurvey(surveyNumb, m_randNumb, m_names, m_age, m_streaming, m_gaming, m_college, m_program, Students);
+	}
 	//Destructor
-
+	~Survey() = default;
 	//Getters
-
+	int getSurveyNumb();
 	//Setters
-
+	void setSurveyNumb(int survey);
+	void setSurvey(int surveyNumb, int randNumb, string names[], int ages[], string streaming[], string gaming[], string college[], string program[], Student* students[]);
+	void DisplaySurvey();
 private:
+	Student* Students[1] = { nullptr };
+	int m_surveyNumber;
+	int m_randNumb;
+	string m_names[10] = {"John", "Bob", "Cameron", "Patrick", "Valentine", "Wallace", "Joss", "Tom", "Alex", "Doom Slayer"};
+	string m_streaming[5] = { "Netflix", "Disney+", "YouTube", "TikTok", "Hulu" };
+	string m_gaming[4] = { "Xbox One X", "Playstation 4", "Nintendo Switch", "PC" };
+	string m_college[5] = { "George Brown", "Centennial", "Seneca", "Algonquin", "Fanshawe" };
+	string m_program[6] = { "Game Developement", "Game Design", "Game Programming", "Business Financing", "Genetics", "Architecture" };
+	int m_age[5] = { 18, 19, 20, 21, 22 };
 
 };
+
